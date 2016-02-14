d0d1ebe1014dba0e3ed1aba836aff6b795515c5c
Ting Guo <guoting.hust@gmail.com>
2016-02-13 23:24:27 -0800

    2/13/2016

---
+++ 46. Permutations.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        boolean[] visited = new boolean[nums.length];
+        helper(result, list, nums, visited);
+        return result;
+    }
+    
+    public void helper(List<List<Integer>> result, List<Integer> list, int[] nums, boolean[] visited){
+        if(list.size()==nums.length){
+            result.add(new ArrayList<Integer>(list));  //！！！！！！！！！
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(!visited[i]) {
+                list.add(nums[i]);
+                visited[i] = true;
+                helper(result, list, nums, visited);
+                list.remove(list.size()-1);
+                visited[i] = false;
+            }
+        }
+    }
+}
